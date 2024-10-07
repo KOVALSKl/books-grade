bc6fdcbf207e08b6723089ee0c4dffe1d36a64fe
KOVALSKl <bagdanmihaenko@yandex.ru>
2024-10-07 10:59:40 +0300

    feat: доработка настроек логгирования

--- home/settings.py
+++ home/settings.py
@@ -14,6 +14,17 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+LOG_DIR = os.path.join(BASE_DIR, 'logs')
+LOG_FILE = '/debug.log'
+LOG_PATH = LOG_DIR + LOG_FILE
+
+if not os.path.exists(LOG_DIR):
+    os.mkdir(LOG_DIR)
+
+if not os.path.exists(LOG_PATH):
+    f = open(LOG_PATH, 'a').close() #create empty log file
+else:
+    f = open(LOG_PATH,"w").close() #clear log file
 
 
 # Quick-start development settings - unsuitable for production
